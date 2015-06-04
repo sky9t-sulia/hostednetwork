83f18572e6d7c57b04ab70a018ed20777a8d87ef
sky9t-sulia <umudov.s.i13@gmail.com>
2015-06-04 05:30:26 +0300

    Master Commit

---
+++ hostednetwork/VirtualWiFi.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace hostednetwork
+{
+    partial class VirtualWiFi
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VirtualWiFi));
+            this.startStopBtn = new System.Windows.Forms.Button();
+            this.launched = new System.Windows.Forms.Label();
+            this.stopped = new System.Windows.Forms.Label();
+            this.ssidLabel = new System.Windows.Forms.Label();
+            this.pwdLabel = new System.Windows.Forms.Label();
+            this.ssid = new System.Windows.Forms.TextBox();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.languageTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.russianTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.minToTrayTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.startupTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.openConsoleBtn = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.ListBox();
+            this.expandTimer = new System.Windows.Forms.Timer(this.components);
+            this.tray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startStopBtn
+            // 
+            resources.ApplyResources(this.startStopBtn, "startStopBtn");
+            this.startStopBtn.FlatAppearance.BorderSize = 0;
+            this.startStopBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.startStopBtn.Name = "startStopBtn";
+            this.startStopBtn.UseVisualStyleBackColor = true;
+            this.startStopBtn.Click += new System.EventHandler(this.startStopAction);
+            // 
+            // launched
+            // 
+            resources.ApplyResources(this.launched, "launched");
+            this.launched.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.launched.Name = "launched";
+            // 
+            // stopped
+            // 
+            resources.ApplyResources(this.stopped, "stopped");
+            this.stopped.Name = "stopped";
+            // 
+            // ssidLabel
+            // 
+            resources.ApplyResources(this.ssidLabel, "ssidLabel");
+            this.ssidLabel.Name = "ssidLabel";
+            // 
+            // pwdLabel
+            // 
+            resources.ApplyResources(this.pwdLabel, "pwdLabel");
+            this.pwdLabel.Name = "pwdLabel";
+            // 
+            // ssid
+            // 
+            resources.ApplyResources(this.ssid, "ssid");
+            this.ssid.Name = "ssid";
+            this.ssid.TextChanged += new System.EventHandler(this.ssid_entering);
+            // 
+            // passTextBox
+            // 
+            resources.ApplyResources(this.passTextBox, "passTextBox");
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.TextChanged += new System.EventHandler(this.pass_entering);
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsTool,
+            this.aboutTool});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // settingsTool
+            // 
+            resources.ApplyResources(this.settingsTool, "settingsTool");
+            this.settingsTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.settingsTool.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageTool,
+            this.minToTrayTool,
+            this.startupTool});
+            this.settingsTool.Name = "settingsTool";
+            // 
+            // languageTool
+            // 
+            resources.ApplyResources(this.languageTool, "languageTool");
+            this.languageTool.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.languageTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.languageTool.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.englishTool,
+            this.russianTool});
+            this.languageTool.Name = "languageTool";
+            // 
+            // englishTool
+            // 
+            resources.ApplyResources(this.englishTool, "englishTool");
+            this.englishTool.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.englishTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.englishTool.Name = "englishTool";
+            this.englishTool.Click += new System.EventHandler(this.setEnglishLang);
+            // 
+            // russianTool
+            // 
+            resources.ApplyResources(this.russianTool, "russianTool");
+            this.russianTool.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.russianTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.russianTool.Name = "russianTool";
+            this.russianTool.Click += new System.EventHandler(this.setRussianLang);
+            // 
+            // minToTrayTool
+            // 
+            resources.ApplyResources(this.minToTrayTool, "minToTrayTool");
+            this.minToTrayTool.BackColor = System.Drawing.Color.White;
+            this.minToTrayTool.Name = "minToTrayTool";
+            this.minToTrayTool.Click += new System.EventHandler(this.minimizeToTray);
+            // 
+            // startupTool
+            // 
+            resources.ApplyResources(this.startupTool, "startupTool");
+            this.startupTool.BackColor = System.Drawing.Color.White;
+            this.startupTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.startupTool.Name = "startupTool";
+            this.startupTool.Click += new System.EventHandler(this.setStartup);
+            // 
+            // aboutTool
+            // 
+            resources.ApplyResources(this.aboutTool, "aboutTool");
+            this.aboutTool.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.aboutTool.Name = "aboutTool";
+            this.aboutTool.Click += new System.EventHandler(this.aboutMessage);
+            // 
+            // openConsoleBtn
+            // 
+            resources.ApplyResources(this.openConsoleBtn, "openConsoleBtn");
+            this.openConsoleBtn.FlatAppearance.BorderSize = 0;
+            this.openConsoleBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.openConsoleBtn.Name = "openConsoleBtn";
+            this.openConsoleBtn.UseVisualStyleBackColor = true;
+            this.openConsoleBtn.Click += new System.EventHandler(this.consoleButton_Click);
+            // 
+            // console
+            // 
+            resources.ApplyResources(this.console, "console");
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.console.Name = "console";
+            // 
+            // expandTimer
+            // 
+            this.expandTimer.Interval = 2;
+            this.expandTimer.Tick += new System.EventHandler(this.expand_Console);
+            // 
+            // tray
+            // 
+            this.tray.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            resources.ApplyResources(this.tray, "tray");
+            this.tray.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayDoubleClick);
+            // 
+            // VirtualWiFi
+            // 
+            this.AcceptButton = this.startStopBtn;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.openConsoleBtn);
+            this.Controls.Add(this.passTextBox);
+            this.Controls.Add(this.ssid);
+            this.Controls.Add(this.pwdLabel);
+            this.Controls.Add(this.ssidLabel);
+            this.Controls.Add(this.stopped);
+            this.Controls.Add(this.launched);
+            this.Controls.Add(this.startStopBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "VirtualWiFi";
+            this.Opacity = 0.99D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.saveSettings);
+            this.Load += new System.EventHandler(this.formLoading);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startStopBtn;
+        private System.Windows.Forms.Label launched;
+        private System.Windows.Forms.Label stopped;
+        private System.Windows.Forms.Label ssidLabel;
+        private System.Windows.Forms.Label pwdLabel;
+        private System.Windows.Forms.TextBox ssid;
+        private System.Windows.Forms.TextBox passTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsTool;
+        private System.Windows.Forms.ToolStripMenuItem languageTool;
+        private System.Windows.Forms.ToolStripMenuItem englishTool;
+        private System.Windows.Forms.ToolStripMenuItem russianTool;
+        private System.Windows.Forms.ToolStripMenuItem aboutTool;
+        private System.Windows.Forms.Button openConsoleBtn;
+        private System.Windows.Forms.ListBox console;
+        private System.Windows.Forms.Timer expandTimer;
+        private System.Windows.Forms.ToolStripMenuItem minToTrayTool;
+        private System.Windows.Forms.ToolStripMenuItem startupTool;
+        private System.Windows.Forms.NotifyIcon tray;
+    }
+}
+
---
+++ hostednetwork/VirtualWiFi.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Windows.Forms;
+
+namespace hostednetwork
+{
+    public partial class VirtualWiFi : Form
+    {
+        public VirtualWiFi()
+        {
+            InitializeComponent();
+        }
+
+        public void Init() {
+            tray.Visible = false;
+            if (Properties.Settings.Default.isStartup == true) {
+                startupTool.CheckState = CheckState.Checked;
+            }
+            ssid.Text = Properties.Settings.Default.ssid;
+            passTextBox.Text = Properties.Settings.Default.password;
+            var lang = Properties.Settings.Default.currentLang;
+            setLanguage(lang);
+        }
+
+        private void Launch(string ssid, string pass) {
+            ExecuteCommand("@echo Resetting hosted network...");
+            ExecuteCommand("netsh wlan set hostednetwork mode = disallow");
+            ExecuteCommand("netsh wlan set hostednetwork mode = allow");
+            ExecuteCommand("@echo Hosted network resetted...");
+            ExecuteCommand("@echo Starting hosted network...");
+            ExecuteCommand("netsh wlan set hostednetwork ssid = " + ssid + " key = " + pass);
+            ExecuteCommand("netsh wlan start hostednetwork");
+            console.Items.Add("********************************************************************");
+            console.Items.Add("");
+            Properties.Settings.Default.isLaunched = true;
+            startStopBtn.Text = getTextFromRes("launchStop", Properties.Settings.Default.currentLang);
+            launched.Text = getTextFromRes("launched", Properties.Settings.Default.currentLang);
+            stopped.Text = "";
+        }
+
+        private void Stop() {
+            ExecuteCommand("@echo Stopping hosted network...");
+            ExecuteCommand("netsh wlan stop hostednetwork");
+            console.Items.Add("********************************************************************");
+            console.Items.Add("");
+            Properties.Settings.Default.isLaunched = false;
+            startStopBtn.Text = getTextFromRes("launchStart", Properties.Settings.Default.currentLang);
+            stopped.Text = getTextFromRes("stopped", Properties.Settings.Default.currentLang);
+            launched.Text = "";
+        }
+
+
+        public void ExecuteCommand(string command) {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            console.Items.Add((String.IsNullOrEmpty(output) ? "(none)" : output));
+            console.Items.Add((String.IsNullOrEmpty(error) ? "" : error));
+            process.Close();
+        }
+
+        private void setLanguage(string lang) {
+            if(lang == "") { lang = "english"; }
+
+            if (Properties.Settings.Default.isLaunched) {
+                stopped.Text = "";
+                launched.Text = getTextFromRes("launched", lang);
+                startStopBtn.Text = getTextFromRes("launchStop", lang);
+            } else {
+                launched.Text = "";
+                stopped.Text = getTextFromRes("stopped", lang);
+                startStopBtn.Text = getTextFromRes("launchStart", lang);
+            }
+            startupTool.Text = getTextFromRes("startuptool", lang);
+            minToTrayTool.Text = getTextFromRes("hidetotraytool", lang);
+            ssidLabel.Text = getTextFromRes("ssidlabel", lang);
+            pwdLabel.Text = getTextFromRes("pwdlabel", lang);
+            settingsTool.Text = getTextFromRes("settingstool", lang);
+            languageTool.Text = getTextFromRes("languagetool", lang);
+            aboutTool.Text = getTextFromRes("abouttool", lang);
+            openConsoleBtn.Text = getTextFromRes("console", lang);
+        }
+
+        private string getTextFromRes(string key, string lang) {
+            if(lang == "") { lang = "english"; }
+            ResourceManager rm = new ResourceManager("hostednetwork." + lang, GetType().Assembly);
+            var entry =
+                rm.GetResourceSet(System.Threading.Thread.CurrentThread.CurrentCulture, true, true)
+                  .OfType<DictionaryEntry>()
+                  .FirstOrDefault(e => e.Key.ToString() == key);
+            var value = entry.Value.ToString();
+            return value;
+        }
+
+        private void setRussianLang(object sender, EventArgs e) {
+            Properties.Settings.Default.currentLang = "russian";
+            setLanguage("russian");
+        }
+
+        private void setEnglishLang(object sender, EventArgs e) {
+            Properties.Settings.Default.currentLang = "english";
+            setLanguage("english");
+        }
+
+        private void saveSettings(object sender, FormClosingEventArgs e) {
+            Properties.Settings.Default.isExpanded = false;
+            Properties.Settings.Default.Save(); // Сохранить настройки
+        }
+
+        private void startStopAction(object sender, EventArgs e) {
+            if(ssid.Text.Length < 3) { ssid.BackColor = Color.Red; return; }
+            if(passTextBox.Text.Length < 8) { passTextBox.BackColor = Color.Red; return; }
+            var isLaunched = Properties.Settings.Default.isLaunched;
+            if (!isLaunched) {
+                var ssid = Properties.Settings.Default.ssid;
+                var pwd =  Properties.Settings.Default.password;
+                Launch(ssid, pwd);
+            } else {
+                Stop();
+            }
+        }
+
+        private void ssid_entering(object sender, EventArgs e) {
+            if(this.Text.Length > 3) { ssid.BackColor = SystemColors.Window; }
+            Properties.Settings.Default.ssid = ssid.Text;
+        }
+
+        private void pass_entering(object sender, EventArgs e) {
+            if(passTextBox.Text.Length >= 8 && passTextBox.BackColor != SystemColors.Window) {
+                passTextBox.BackColor = SystemColors.Window;
+            }
+            Properties.Settings.Default.password = passTextBox.Text;
+        }
+
+        private void formLoading(object sender, EventArgs e) {
+            Init();
+        }
+
+        private void consoleButton_Click(object sender, EventArgs e) {
+            expandTimer.Start();
+        }
+
+        private void expand_Console(object sender, EventArgs e) {
+            if (Properties.Settings.Default.isExpanded == false) {
+                if (this.Width <= 1200) {
+                    this.Width += 40;
+                } else {
+                    Properties.Settings.Default.isExpanded = true;
+                    expandTimer.Stop();
+                }
+            } else {
+                if (this.Width > 299) {
+                    this.Width -= 40;
+                } else {
+                    Properties.Settings.Default.isExpanded = false;
+                    expandTimer.Stop();
+                }
+            }
+        }
+
+        private void aboutMessage(object sender, EventArgs e) {
+            var msg = MessageBox.Show(getTextFromRes("about",Properties.Settings.Default.currentLang),"VirtualWiFi",MessageBoxButtons.OK);
+        }
+
+        private void setStartup(object sender, EventArgs e) {
+            // The path to the key where Windows looks for startup applications
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (Properties.Settings.Default.isStartup == true) {
+                startupTool.CheckState = CheckState.Unchecked;
+                Properties.Settings.Default.isStartup = false;
+                // Remove the value from the registry so that the application doesn't start
+                rkApp.DeleteValue("VirtualWiFi", false);
+
+            } else {
+                startupTool.CheckState = CheckState.Checked;
+                Properties.Settings.Default.isStartup = true;
+                // Add the value in the registry so that the application runs at startup
+                rkApp.SetValue("VirtualWiFi", Application.ExecutablePath.ToString());
+            }
+        }
+        bool firstTime = true;
+        private void minimizeToTray(object sender, EventArgs e) {
+            this.ShowInTaskbar = false;
+            tray.Visible = true;
+            if (firstTime) {
+                tray.ShowBalloonTip(2000);
+                firstTime = false;
+            }
+            this.Hide();
+        }
+
+        private void trayDoubleClick(object sender, MouseEventArgs e) {
+            this.ShowInTaskbar = true;
+            tray.Visible = false;
+            this.Show();
+        }
+    }
+}
